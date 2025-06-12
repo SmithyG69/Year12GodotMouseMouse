27673336e3fcae3ba5838f8047ed385defb1d804
SmithyG69 <flecla49@kingshigh.school.nz>
2025-06-12 22:54:25 +1200

    Proof of concept spawning on hub0

--- connors-mitosis-delivery-service/HubScript.gd
+++ connors-mitosis-delivery-service/HubScript.gd
@@ -1,19 +1,27 @@
 extends Node3D
 
-var hub_id: int
-var data: Dictionary = {}
+@export var model_to_clone: PackedScene  # Assign your 3D model scene (e.g. Crate.tscn)
+@onready var cloning_area = $CloningArea
+@onready var start_button = $UIContainer/CanvasLayer/Control/VBoxContainer/start
+@onready var timer = $CloningArea/Timer
 
+var clone_count = 50  # how many to spawn
+var spacing = 1    # space between them
 
 func _ready():
-	if data:
-		print("Loaded Hub ID:", data.hub_id)
-		
+	start_button.pressed.connect(_on_start_button_pressed)
 
-
+func _on_start_button_pressed():
+	for i in range(clone_count):
+		var clone = model_to_clone.instantiate()
+		var pos = Vector3(i * spacing, randi_range(0.01,0.1), randi_range(0.01,0.1))
+		clone.position = pos
+		cloning_area.add_child(clone)
+		clone.scale = Vector3(0.1,0.1,0.1)
 #func _process(delta):
 #	if data:
 		#data.process_clone(delta)
 
 
-func _on_button_pressed() -> void:
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
